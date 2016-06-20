608c88502eb25859091145f5722195f4124430d2
David Pello <zako@webservervieju.net>
2016-06-20 20:59:22 +0200

    small fixes, heading icon in config

--- config.php
+++ config.php
@@ -3,6 +3,7 @@
 		"telem_file" => "telem-ns1.txt",
 		"logo_img" => "img/ashab.png",
 		"payload_icon" => "img/icono-ns1.png",
+		"arrow_icon" => "img/heading-arrow.png",
 		"get_data_url" => "http://ashab.space/tracker/get_ns1_data.php"
 	);
 ?>
